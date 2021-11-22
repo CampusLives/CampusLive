c4f84dec449081ca3ab5a3a9cd461811fbe1f035
koiralasasbit <koiralasasbit@gmail.com>
2021-11-22 11:21:55 -0500

    Project created

---
+++ CampusLives/CampusLives/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusLives
+//
+//  Created by Sasbit Koirala on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

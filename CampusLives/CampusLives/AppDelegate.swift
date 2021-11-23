b9f592d9c925c939650e927b05f1c06e9498dda6
koiralasasbit <koiralasasbit@gmail.com>
2021-11-22 21:00:33 -0500

    Parser server setup

--- CampusLives/CampusLives/AppDelegate.swift
+++ CampusLives/CampusLives/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "IrDGNMsYiNWdw5gTA6QXHj9Ozo4kAY9LT9VJTfNC" // <- UPDATE
+                    $0.clientKey = "BJFtnh5HXESFkpCFUVRaYLObZL41NsnBWpmyrz1j" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
